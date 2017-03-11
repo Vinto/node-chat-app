76cd8c918b4f410ebfe0309ef59f683c26d5a7cf
Vinto <vintogen@yahoo.co.uk>
2017-03-11 23:16:04 +0200

    User message updated

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.on('disconnect', () => {
-    console.log('Disconnected from client');
+    console.log('User was disconnected');
   });
 });
 
